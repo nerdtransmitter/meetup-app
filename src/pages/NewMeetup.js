2d833fdd566e0284e425585138f7e26d36ffc194
Alessia Moison <adm2217@columbia.edu>
2023-04-28 19:46:56 +0200

    form and post fetch

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://meetup-app-d7f69-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  }
+
   return (
-    <div>New Meetup Page</div>
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+    </section>
   )
 }
 
