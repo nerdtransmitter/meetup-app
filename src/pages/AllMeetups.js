6aaf5e494f2a574354b7db9722033bc069344893
Alessia Moison <adm2217@columbia.edu>
2023-05-01 14:04:37 +0200

    fetching data from firebase

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
+function AllMeetUpsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadingMeetups, setLoadingMeetups] = useState([]);
 
-function AllMeetUpsPage(me) {
+  useEffect(() => {
+    fetch(
+      'https://meetup-app-d7f69-default-rtdb.firebaseio.com/meetups.json',
+    ).then(response => {
+      return response.json();
+    }).then((data) => {
+      const meetups = [];
+
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key]
+        }
+
+        meetups.push(meetup);
+      };
+
+      setIsLoading(false);
+      setLoadingMeetups(meetups);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>is loading...</p>
+      </section>
+    )
+  }
+
   return (
     // <Container maxWidth="sm">
     <div>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadingMeetups} />
     </div>
     /* </Container> */
   );
