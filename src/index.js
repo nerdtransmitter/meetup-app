423a88d784d68efb5e3d6269b350289145c51f99
Alessia Moison <adm2217@columbia.edu>
2023-05-01 16:08:06 +0200

    add to favorites now adds meetup to favorite page and updates the count on badge in navbar

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './index.css';
 import App from './App';
 import { FavoritesContextProvider } from './store/favorites-context';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <FavoritesContextProvider>
